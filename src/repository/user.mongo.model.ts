454e69fe947a5ba99eecd447e401a2e3abade443
Jesus Alvarez <jesusalvarezaguilar94@gmail.com>
2023-09-02 17:24:17 +0200

    Add the mongo.model file

---
+++ src/repository/user.mongo.model.ts
@@ -0,0 +1,40 @@
+import { Schema, model } from 'mongoose';
+import { User } from '../entities/user';
+
+const userSchema = new Schema<User>({
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  enemies: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+});
+
+userSchema.set('toJSON', {
+  transform(_document, returnedObject) {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.password;
+  },
+});
+
+export const UserModel = model('User', userSchema, 'users');
