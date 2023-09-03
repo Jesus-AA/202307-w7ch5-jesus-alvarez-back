de848570763eaa88916be8699bc8e43c0a5d639b
Jesus Alvarez <jesusalvarezaguilar94@gmail.com>
2023-09-03 13:19:20 +0200

    Add authorization required to getall

--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -1,11 +1,13 @@
 import createDebug from 'debug';
 import { Router as createRouter } from 'express';
 import { UserController } from '../controller/users.controller.js';
+import { AuthInterceptor } from '../middleware/auth.interceptor.js';
 import { UserMongoRepository } from '../repository/user.mongo.repository.js';
 
 const debug = createDebug('V25:Router: UserRouter');
 
 debug('Loaded');
+const authInterceptor = new AuthInterceptor();
 
 const repo = new UserMongoRepository();
 const userController = new UserController(repo);
@@ -14,7 +16,11 @@ export const userRouter = createRouter();
 userRouter.patch('/login', userController.login.bind(userController));
 userRouter.post('/register', userController.create.bind(userController));
 
-userRouter.get('/', userController.getAll.bind(userController));
+userRouter.get(
+  '/',
+  authInterceptor.authorization.bind(authInterceptor),
+  userController.getAll.bind(userController)
+);
 userRouter.get('/:id', userController.getById.bind(userController));
 
 userRouter.patch('/:id', userController.update.bind(userController));
